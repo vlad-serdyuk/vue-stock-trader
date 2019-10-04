432d414f17b2c7b733c440b324f5be8506f2cb86
vlad.serdyuk <vladserduk@live.com>
2019-10-04 16:06:01 +0300

    VSTR-2: [feature] Add routing and header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="container">
-
+        <Header />
+        <div class="row">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
 <script>
-
-    export default {
-
-    }
+import Header from './components/Header.vue';
+    
+ export default {
+     components: {
+         Header,
+     }
+ }
 </script>
 
 <style>
-
+body {
+    padding: 0 30px;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>The Header Component</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <router-link to='/' class="navbar-brand">Stock Trader</router-link>
+
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <router-link to='/portfolio' tag="li" class="nav-item" activeClass="active"><a class="nav-link">Portfolio</a></router-link>
+                <router-link to='/stocks' tag="li" class="nav-item" activeClass="active"><a class="nav-link">Stocks</a></router-link>
+            </ul>
+        </div>
+    </nav>
 </template>
