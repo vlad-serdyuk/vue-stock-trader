9b5063af80c44db4126d5972102d56be6103fed0
vlad.serdyuk <vladserduk@live.com>
2019-10-07 10:49:39 +0300

    VSTR-3: [feature] Add stocks

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <Stock 
+            v-for="stock in stocks"
+            :stock="stock"
+            :key="stock.id"
+        />
+    </div>
+</template>
+<script>
+import Stock from './Stock.vue';
+
+export default {
+    components: {
+        Stock,
+    },
+    data() {
+        return {
+            stocks: [
+                {
+                    id: 1,
+                    name: 'Apple',
+                    price: 100,
+                },
+                {
+                    id: 2,
+                    name: 'Goggle',
+                    price: 100,
+                },
+            ],
+        };
+    },
+}
+</script>
