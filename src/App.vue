03ffd1965641e6aae0be9ca0ecb70c25a29f94ef
vlad.serdyuk <vladserduk@live.com>
2019-10-11 14:21:54 +0300

    VSTR-7: [feature] Add transition and endDay

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,60 @@
 <template>
-    <div class="container">
-        <Header />
-        <div class="row">
-            <router-view></router-view>
-        </div>
+  <div class="container">
+    <Header />
+    <div class="row">
+      <transition name="slide" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </div>
+  </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
     
- export default {
-     components: {
-         Header,
-     },
-     created() {
-         this.$store.dispatch('initStocks');
-     },
+export default {
+  components: {
+    Header,
+  },
+  created() {
+    this.$store.dispatch('initStocks');
+  },
  }
 </script>
 
 <style>
 body {
-    padding: 0 30px;
+  padding: 0 30px;
 }
+
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+      transform: translateY(-30px);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,14 +7,42 @@
                 <router-link to='/portfolio' tag="li" class="nav-item" activeClass="active"><a class="nav-link">Portfolio</a></router-link>
                 <router-link to='/stocks' tag="li" class="nav-item" activeClass="active"><a class="nav-link">Stocks</a></router-link>
             </ul>
+            <a href="#" @click="endDay">End Day</a>
+            <div 
+              class="dropdown"
+              :class="{ open: isDropdownOpen }"
+              @click="onDropdownClick"
+            >
+              <a>
+                <ul class="dropdown-menu">
+                  <li>Save data</li>
+                  <li>Load data</li>
+                </ul>
+              </a>
+            </div>
             <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
         </div>
     </nav>
 </template>
 <script>
+import { mapActions } from 'vuex';
+
 export default {
+    data() {
+      isDropdownOpen: false,
+    },
     computed: {
-        return this.$store.getters.funds;
+      return this.$store.getters.funds;
+    },
+    methods: {
+      ...mapActions(['randomizeStocks']),
+      endDay() {
+        this.randomizeStocks();
+      },
+
+      onDropdownClick() {
+        this.isDropdownOpen = !this.isDropdownOpen;
+      },
     },
 }
 </script>
